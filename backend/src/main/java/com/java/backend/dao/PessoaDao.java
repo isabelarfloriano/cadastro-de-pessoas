49b05b240b8a6e223e75bf20bfa4e54ff0da06c5
Isabela Rozani Floriano <irf161295@gmail.com>
2023-04-26 20:07:43 -0300

    refactor: ajusta conexão com o banco de dados

--- backend/src/main/java/com/java/backend/dao/PessoaDao.java
+++ backend/src/main/java/com/java/backend/dao/PessoaDao.java
@@ -1,6 +1,7 @@
 package com.java.backend.dao;
 
 import java.util.List;
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,15 +15,22 @@ import com.java.backend.model.Pessoa;
 @Component
 public class PessoaDao {
 
-    private final JdbcTemplate jdbcTemplate;
+  private final JdbcTemplate jdbcTemplate;
+  private final String createTableSql;
 
-    @Autowired
-    public PessoaDao(DataBaseConfig dataBaseConfig) {
-        this.jdbcTemplate = new JdbcTemplate(dataBaseConfig.dataSource());
-    }
+  @Autowired
+  public PessoaDao(DataSource dataSource, DataBaseConfig dataBaseConfig) {
+      this.jdbcTemplate = new JdbcTemplate(dataSource);
+      this.createTableSql = dataBaseConfig.criaTabelaSql();
+      init();
+  }
+
+  private void init() {
+      jdbcTemplate.execute(createTableSql);
+  }
 
     public List<Pessoa> listarPessoas() {
-        String sql = "SELECT * FROM pessoa";
+        String sql = "SELECT * FROM pessoas_cadastradas";
         return jdbcTemplate.query(sql, (rs, rowNum) -> {
             Pessoa pessoa = new Gson().fromJson(rs.getString("json"), Pessoa.class);
             pessoa.setId(rs.getInt("id"));
@@ -34,17 +42,17 @@ public class PessoaDao {
         int id = gerarId();
         pessoa.setId(id);
         String json = new Gson().toJson(pessoa);
-        String sql = "INSERT INTO pessoa (id, json) VALUES (?, ?)";
+        String sql = "INSERT INTO pessoas_cadastradas (id, json) VALUES (?, ?)";
         jdbcTemplate.update(sql, id, json);
     }
 
     public void deletarPessoa(int id) {
-        String sql = "DELETE FROM pessoa WHERE id = ?";
+        String sql = "DELETE FROM pessoas_cadastradas WHERE id = ?";
         jdbcTemplate.update(sql, id);
     }
 
     private int gerarId() {
-        String sql = "SELECT MAX(id) FROM pessoa";
+        String sql = "SELECT MAX(id) FROM pessoas_cadastradas";
         Integer maxId = jdbcTemplate.queryForObject(sql, Integer.class);
         if (maxId == null) {
             return 1;
