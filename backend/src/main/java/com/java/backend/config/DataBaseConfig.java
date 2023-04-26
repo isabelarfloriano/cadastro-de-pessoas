5894df697a1cf30c9621ce2361463d6543e70e48
Isabela Rozani Floriano <irf161295@gmail.com>
2023-04-26 20:06:46 -0300

    refactor: ajusta configurações para criar a tabela ao iniciar a aplicação

--- backend/src/main/java/com/java/backend/config/DataBaseConfig.java
+++ backend/src/main/java/com/java/backend/config/DataBaseConfig.java
@@ -1,7 +1,9 @@
 package com.java.backend.config;
 
 import javax.sql.DataSource;
-import org.springframework.context.annotation.*;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -10,17 +12,27 @@ public class DataBaseConfig {
 
   @Bean
   public DataSource dataSource() {
-      DriverManagerDataSource dataSource = new DriverManagerDataSource();
-      dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-      dataSource.setUrl("jdbc:mysql://localhost:3306/db?useSSL=false&createDatabaseIfNotExist=true");
-      dataSource.setUsername("root");
-      dataSource.setPassword("password");
-      return dataSource;
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+    dataSource.setUrl("jdbc:mysql://localhost:3306/db?useSSL=false&createDatabaseIfNotExist=true");
+    dataSource.setUsername("root");
+    dataSource.setPassword("password");
+    return dataSource;
   }
 
   @Bean
-  public JdbcTemplate jdbcTemplate(DataSource dataSource) {
-      return new JdbcTemplate(dataSource);
-  }
-  
+  public JdbcTemplate jdbcTemplate() {
+    return new JdbcTemplate(dataSource());
 }
+
+  @Bean
+  public String criaTabelaSql() {
+      return "CREATE TABLE IF NOT EXISTS pessoas_cadastradas (\n" +
+              "  id INT PRIMARY KEY AUTO_INCREMENT,\n" +
+              "  nome VARCHAR(50) NOT NULL,\n" +
+              "  idade INT NOT NULL,\n" +
+              "  email VARCHAR(50) NOT NULL UNIQUE\n" +
+              ")";
+  }
+}
+
