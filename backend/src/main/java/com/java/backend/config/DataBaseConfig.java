b79dc8a8e330202db09751e88ed4c54ed5727897
Isabela Rozani Floriano <irf161295@gmail.com>
2023-04-26 15:02:40 -0300

    feat: define conexão com banco de dados utilizando jdbc

--- backend/src/main/java/com/java/backend/config/DataBaseConfig.java
+++ backend/src/main/java/com/java/backend/config/DataBaseConfig.java
@@ -1,5 +1,26 @@
 package com.java.backend.config;
 
+import javax.sql.DataSource;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
 public class DataBaseConfig {
 
+  @Bean
+  public DataSource dataSource() {
+      DriverManagerDataSource dataSource = new DriverManagerDataSource();
+      dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+      dataSource.setUrl("jdbc:mysql://localhost:3306/db?useSSL=false&createDatabaseIfNotExist=true");
+      dataSource.setUsername("root");
+      dataSource.setPassword("password");
+      return dataSource;
+  }
+
+  @Bean
+  public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+      return new JdbcTemplate(dataSource);
+  }
+  
 }
