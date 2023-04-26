999a2bb4f5e8a7c9da11eef9be0434211b6d9a49
Isabela Rozani Floriano <irf161295@gmail.com>
2023-04-26 15:37:27 -0300

    feat: adiciona ao controller tratamento de exceção e retornos HTTP

--- backend/src/main/java/com/java/backend/controller/CadastroDePessoasController.java
+++ backend/src/main/java/com/java/backend/controller/CadastroDePessoasController.java
@@ -1,8 +1,12 @@
 package com.java.backend.controller;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.java.backend.dao.PessoaDao;
+import com.java.backend.exception.PessoaNotFoundException;
 import com.java.backend.model.Pessoa;
 
 @RestController
@@ -11,22 +15,33 @@ public class CadastroDePessoasController {
 
   private final PessoaDao pessoaDao;
 
+  @Autowired
   public CadastroDePessoasController(PessoaDao pessoaDao) {
       this.pessoaDao = pessoaDao;
   }
 
   @GetMapping
-  public List<Pessoa> listarPessoasCadastradas() {
-      return pessoaDao.listarPessoas();
+  public ResponseEntity<List<Pessoa>> listarPessoasCadastradas() {
+      List<Pessoa> pessoas = pessoaDao.listarPessoas();
+      if (pessoas.isEmpty()) {
+          return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+      }
+      return new ResponseEntity<>(pessoas, HttpStatus.OK);
   }
 
   @PostMapping
-  public void cadastrarPessoa(@RequestBody Pessoa pessoa) {
+  public ResponseEntity<Pessoa> cadastrarPessoa(@RequestBody Pessoa pessoa) {
       pessoaDao.cadastrarPessoa(pessoa);
+      return new ResponseEntity<>(pessoa, HttpStatus.CREATED);
   }
 
   @DeleteMapping("/{id}")
-  public void deletarPessoa(@PathVariable("id") int id) {
-      pessoaDao.deletarPessoa(id);
+  public ResponseEntity<Void> deletarPessoa(@PathVariable int id) {
+      try {
+          pessoaDao.deletarPessoa(id);
+          return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+      } catch (PessoaNotFoundException e) {
+          return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+      }
   }
 }
