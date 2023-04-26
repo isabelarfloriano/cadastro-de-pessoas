bbbeb942d2a949dbe3da452d86d6f9ad9d9038e3
Isabela Rozani Floriano <irf161295@gmail.com>
2023-04-26 12:38:26 -0300

    fix: indentação

--- backend/src/main/java/com/java/backend/CadastroDePessoasApplication.java
+++ backend/src/main/java/com/java/backend/CadastroDePessoasApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CadastroDePessoasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CadastroDePessoasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroDePessoasApplication.class, args);
+    }
 
 }
