490d2ea9e3842b7cfce40ec55af7ae83fd3a0aed
Isabela Rozani Floriano <irf161295@gmail.com>
2023-04-26 20:09:11 -0300

    refactor: adiciona especificação de scan do Spring

--- backend/src/main/java/com/java/backend/CadastroDePessoasApplication.java
+++ backend/src/main/java/com/java/backend/CadastroDePessoasApplication.java
@@ -2,8 +2,10 @@ package com.java.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.java.backend")
 public class CadastroDePessoasApplication {
 
     public static void main(String[] args) {
