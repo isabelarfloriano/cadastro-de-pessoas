4a567ea3479bd08d5c2883ad7e1288e4190f170d
Isabela Rozani Floriano <irf161295@gmail.com>
2023-04-27 01:57:36 -0300

    feat: CORS - cria configuração para permitir solicitações de diferentes origens

---
+++ backend/src/main/java/com/java/backend/config/CorsConfig.java
@@ -0,0 +1,19 @@
+package com.java.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// Fonte: https://www.baeldung.com/spring-cors
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+            .allowedMethods("GET", "POST", "DELETE")
+            .allowedOrigins("*");
+    }
+}
