7b70f258c9b573119bf88efbc99a2d8c600a44e4
Isabela Rozani Floriano <irf161295@gmail.com>
2023-04-26 23:06:20 -0300

    fix: declaração da tabela pessoas_cadastradas

--- backend/src/main/java/com/java/backend/config/DataBaseConfig.java
+++ backend/src/main/java/com/java/backend/config/DataBaseConfig.java
@@ -16,7 +16,7 @@ public class DataBaseConfig {
     dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
     dataSource.setUrl("jdbc:mysql://localhost:3306/db?useSSL=false&createDatabaseIfNotExist=true");
     dataSource.setUsername("root");
-    dataSource.setPassword("password");
+    dataSource.setPassword("s.e.n.h.a");
     return dataSource;
   }
 
@@ -30,8 +30,9 @@ public class DataBaseConfig {
       return "CREATE TABLE IF NOT EXISTS pessoas_cadastradas (\n" +
               "  id INT PRIMARY KEY AUTO_INCREMENT,\n" +
               "  nome VARCHAR(50) NOT NULL,\n" +
-              "  idade INT NOT NULL,\n" +
-              "  email VARCHAR(50) NOT NULL UNIQUE\n" +
+              "  email VARCHAR(50) NOT NULL,\n" +
+              "  telefone VARCHAR(50) NOT NULL,\n" +
+              "  profissao VARCHAR(50) NOT NULL\n" +
               ")";
   }
 }
