01c1daf4450dba483cbc15c73553e2e39d79552b
Isabela Rozani Floriano <irf161295@gmail.com>
2023-04-27 13:59:56 -0300

    fix: sintaxe

--- backend/src/main/java/com/java/backend/config/CorsConfig.java
+++ backend/src/main/java/com/java/backend/config/CorsConfig.java
@@ -5,14 +5,16 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-// Fonte: https://www.baeldung.com/spring-cors
+/**
+* Fonte: https://www.baeldung.com/spring-cors
+**/
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
             .allowedMethods("GET", "POST", "DELETE")
             .allowedOrigins("*");
     }
